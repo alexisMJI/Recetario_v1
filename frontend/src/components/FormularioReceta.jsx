7420ec8ead4c6dfe591d1e9f352700561cec9e50
alexisMJI <alexismarin11@hotmail.es>
2022-05-28 00:24:17 -0300

    m3

--- frontend/src/components/FormularioReceta.jsx
+++ frontend/src/components/FormularioReceta.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 
 const FormularioReceta = () => {
   
-  const [titulo,setTitle] = useState('')
+  const [title,setTitle] = useState('')
   const [ingredients,setIngredients] = useState('')
   const [preparation,setPreparation] = useState('')
   const [date_creation,setDate_creation] = useState('')
@@ -14,8 +14,14 @@ const FormularioReceta = () => {
     <form className='bg-white py-10 px-5 md: w-1/2 rounded-lg shadow-xl'>
       <div>
         <label className='text-gray-700 uppercase font-bold text-sm' htmlFor='nombre'>Titulo de Receta</label>
-        <input id='title' type="text" className='border w-full p-2 mt-2 placeholder-gray-400 rounded-md' value={title} onChange={(e)=> setTitle(e.target.value)}/>
+        <input id='title' type="text" className='border w-full p-2 mt-2 placeholder-gray-400 rounded-md' placeholder='Titulo de la Receta' value={title} onChange={(e)=> setTitle(e.target.value)}/>
+        
+        <label className='text-gray-700 uppercase font-bold text-sm' htmlFor='nombre'>Titulo de Receta</label>
+        <input id='ingredients' type="text" className='border w-full p-2 mt-2 placeholder-gray-400 rounded-md' placeholder='Ingredientes de la Receta' value={ingredients} onChange={(e)=> setIngredients(e.target.value)}/>
 
+        <label className='text-gray-700 uppercase font-bold text-sm' htmlFor='nombre'>Titulo de Receta</label>
+        <input id='preparation' type="text" className='border w-full p-2 mt-2 placeholder-gray-400 rounded-md' placeholder='Preparacion de la Receta' value={preparation} onChange={(e)=> setPreparation(e.target.value)}/>
+        
 
       </div>
     </form>
